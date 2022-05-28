7fc85ae2ff20753ba120cae9da429c57615cbe6c
Ahmad <ahmedmageed19@gmail.com>
2022-05-28 19:04:06 +0200

    Server Build

--- dist/Middleware/errorHandler.js
+++ dist/Middleware/errorHandler.js
@@ -1,10 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.errorHandler = void 0;
+
 function errorHandler(error, req, res, next) {
-    var status = error.statusCode || 500;
-    var mssg = error.message || 'Something Went Wrong';
-    res.status(status).send({ status: status, mssg: mssg });
-    next();
+  var status = error.statusCode || 500;
+  var mssg = error.message || 'Something Went Wrong';
+  res.status(status).send({ status: status, mssg: mssg });
+  next();
 }
+
 exports.errorHandler = errorHandler;
--- dist/tests/userSpec.js
+++ dist/tests/userSpec.js
@@ -1,10 +1,10 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var User_1 = require("../Models/User");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+var User_1 = require('../Models/User');
 // User Table Testing
-describe('User Table Model Testing', function () {
-    var users = new User_1.UserTable();
-    it('Should have a listAll Method', function () {
-        expect(users.listAll).toBeDefined();
-    });
+describe('User Table Model Testing', function() {
+  var users = new User_1.UserTable();
+  it('Should have a listAll Method', function() {
+    expect(users.listAll).toBeDefined();
+  });
 });
