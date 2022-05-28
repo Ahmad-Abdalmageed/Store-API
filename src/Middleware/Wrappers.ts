1749cd7ccec8575f17e2bcf0d704607d5ad3ef99
Ahmad <ahmedmageed19@gmail.com>
2022-05-28 13:01:02 +0200

    Express Try Catch Wrapper

--- src/Middleware/Wrappers.ts
+++ src/Middleware/Wrappers.ts
@@ -1,10 +1,25 @@
 // Async Function Wrapper
 
-const genericWrap = async (message: string, callback: CallableFunction) => {
+import { NextFunction, Request, Response } from 'express';
+
+const tryCatchWrap = async (message: string, callback: CallableFunction) => {
   try {
     return await callback();
   } catch (e) {
     throw new Error(`${message} -:> ${e}`);
   }
 };
-export { genericWrap };
+
+const tryCatchWrapExpress = (callback: CallableFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // Execute the Passed Callback
+      await callback(req, res, next);
+    } catch (error) {
+      //Catch errors if any
+      next(error);
+    }
+  };
+};
+
+export { tryCatchWrap, tryCatchWrapExpress };
