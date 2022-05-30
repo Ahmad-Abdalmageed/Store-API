b84237f65ad47a1c375aa48cbda8a12db473fd2c
Ahmad <ahmedmageed19@gmail.com>
2022-05-30 20:30:48 +0200

    User Schema Edit

--- migrations/sqls/20220526184719-store-api-up.sql
+++ migrations/sqls/20220526184719-store-api-up.sql
@@ -1,9 +1,10 @@
 ----------------------------------------------------------
 CREATE TABLE Users(
-    id SERIAL NOT NULL,
-    FirstName VARCHAR(20) NOT NULL,
-    LastName VARCHAR(20) NOT NULL,
-    password varchar(20) NOT NULL
+                      id        SERIAL      NOT NULL,
+                      username  VARCHAR(30) NOT NULL,
+                      firstname VARCHAR(20) NOT NULL,
+                      lastname  VARCHAR(20) NOT NULL,
+                      password  VARCHAR     NOT NULL
 );
 ALTER TABLE Users
     ADD CONSTRAINT "USERS_PK"
