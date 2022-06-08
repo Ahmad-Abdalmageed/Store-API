4582a80d72b02f22f6a47f883068fe2ab911791c
Ahmad <ahmedmageed19@gmail.com>
2022-06-08 20:14:31 +0200

    Add uid to locals

--- src/Middleware/auth.ts
+++ src/Middleware/auth.ts
@@ -7,8 +7,8 @@ function authenticate(req: Request, res: Response, next: NextFunction): void {
   try {
     const token = req.headers.authorization?.split(' ')[1] as unknown as string;
     const secret = process.env.TOKEN_SECRET as unknown as string;
-    jwt.verify(token, secret);
-    console.log('Here');
+    const decoded = jwt.verify(token, secret);
+    res.locals.userID = (decoded as jwt.JwtPayload).user.id;
     next();
   } catch (e) {
     return next(new apiError(401, 'Could not Authenticate User'));
