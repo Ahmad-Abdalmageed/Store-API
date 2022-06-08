7098f9b33f10b166999639f37f6b142ecf1aed7f
Ahmad <ahmedmageed19@gmail.com>
2022-06-08 20:13:15 +0200

    Database Schema Edits

--- migrations/sqls/20220526184719-store-api-up.sql
+++ migrations/sqls/20220526184719-store-api-up.sql
@@ -10,48 +10,54 @@ ALTER TABLE Users
     ADD CONSTRAINT "USERS_PK"
         PRIMARY KEY(id);
 ----------------------------------------------------------
-CREATE TABLE Products(
-    id SERIAL NOT NULL,
-    name VARCHAR(100) NOT NULL,
-    price INTEGER NOT NULL,
+CREATE TABLE Products
+(
+    id       SERIAL       NOT NULL,
+    name     VARCHAR(100) NOT NULL,
+    price    INTEGER      NOT NULL,
     category VARCHAR(100) NULL
 );
 ALTER TABLE Products
     ADD CONSTRAINT "Products_PK"
         PRIMARY KEY(id);
 ----------------------------------------------------------
-CREATE TABLE Orders(
-     id SERIAL NOT NULL,
-     user_id INTEGER NOT NULL,
-     status VARCHAR(20) NOT NULL,
-     date DATE NOT NULL
+CREATE TABLE Orders
+(
+    id      SERIAL      NOT NULL,
+    user_id INTEGER     NOT NULL,
+    status  VARCHAR(20) NOT NULL,
+    date    DATE        NOT NULL
 );
 ALTER TABLE Orders
-    ADD CONSTRAINT  "ORDERS_PK"
-        PRIMARY KEY(id);
+    ADD CONSTRAINT "ORDERS_PK"
+        PRIMARY KEY (id);
 
 ALTER TABLE Orders
     ADD CONSTRAINT "ORDERS_`user_id`_FK"
-        FOREIGN KEY(user_id)
-            REFERENCES Users(id);
+        FOREIGN KEY (user_id)
+            REFERENCES Users (id)
+            ON DELETE CASCADE;
 ----------------------------------------------------------
-CREATE TABLE order_product(
-    id SERIAL NOT NULL,
+CREATE TABLE order_product
+(
+    id       SERIAL  NOT NULL,
     order_id INTEGER NOT NULL,
-    prod_id INTEGER NOT NULL,
+    prod_id  INTEGER NOT NULL,
     quantity INTEGER NOT NULL
 );
 ALTER TABLE order_product
     ADD CONSTRAINT "ORDER_PRODUCT_PK"
-        PRIMARY KEY(id);
+        PRIMARY KEY (id);
 
 ALTER TABLE order_product
     ADD CONSTRAINT "ORDER_PRODUCT_`prod_id`_FK"
-        FOREIGN KEY(prod_id)
-            REFERENCES Products(id);
+        FOREIGN KEY (prod_id)
+            REFERENCES Products (id)
+            ON DELETE CASCADE;
 
 ALTER TABLE order_product
     ADD CONSTRAINT "ORDER_PRODUCT_`order_id`_FK"
-        FOREIGN KEY(order_id)
-            REFERENCES Orders(id);
+        FOREIGN KEY (order_id)
+            REFERENCES Orders (id)
+            ON DELETE CASCADE;
 ----------------------------------------------------------
