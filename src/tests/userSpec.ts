463a270a027dc8beee13e317b5dd5f931eb6aa55
Ahmad <ahmedmageed19@gmail.com>
2022-06-09 23:17:45 +0200

    File Rename

--- src/tests/userSpec.ts
+++
@@ -1,9 +0,0 @@
-import { UserTable } from '../Models/User';
-
-// User Table Testing
-describe('User Table Model Testing', () => {
-  const users = new UserTable();
-  it('Should have a listAll Method', () => {
-    expect(users.listAll).toBeDefined();
-  });
-});
