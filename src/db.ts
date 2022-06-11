d1c1872a19ce2a4222607af847ec09a4e9e15368
Ahmad <ahmedmageed19@gmail.com>
2022-06-11 18:11:18 +0200

    Added Port to Connection

--- src/db.ts
+++ src/db.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const {
   POSTGRES_HOST,
+  POSTGRES_PORT,
   POSTGRES_DB,
   POSTGRES_DB_TEST,
   POSTGRES_USER,
@@ -19,6 +20,7 @@ switch (ENV) {
   case 'dev':
     client = new Pool({
       host: POSTGRES_HOST,
+      port: Number(POSTGRES_PORT),
       database: POSTGRES_DB,
       user: POSTGRES_USER,
       password: POSTGRES_PASSWORD
@@ -27,6 +29,7 @@ switch (ENV) {
   case 'test':
     client = new Pool({
       host: POSTGRES_HOST,
+      port: Number(POSTGRES_PORT),
       database: POSTGRES_DB_TEST,
       user: POSTGRES_USER,
       password: POSTGRES_PASSWORD
