eb7813ea4e00a1a988090a1b80672b23c2268755
Ahmad <ahmedmageed19@gmail.com>
2022-06-22 22:22:45 +0200

    Server Build

--- dist/db.js
+++ dist/db.js
@@ -7,7 +7,7 @@ exports.client = void 0;
 var dotenv_1 = __importDefault(require("dotenv"));
 var pg_1 = require("pg");
 dotenv_1.default.config();
-var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_DB_TEST = _a.POSTGRES_DB_TEST, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV;
+var _a = process.env, POSTGRES_HOST = _a.POSTGRES_HOST, POSTGRES_PORT = _a.POSTGRES_PORT, POSTGRES_DB = _a.POSTGRES_DB, POSTGRES_DB_TEST = _a.POSTGRES_DB_TEST, POSTGRES_USER = _a.POSTGRES_USER, POSTGRES_PASSWORD = _a.POSTGRES_PASSWORD, ENV = _a.ENV;
 var client;
 exports.client = client;
 console.log("Connecting to ".concat(ENV, " DB ..."));
@@ -15,6 +15,7 @@ switch (ENV) {
     case 'dev':
         exports.client = client = new pg_1.Pool({
             host: POSTGRES_HOST,
+            port: Number(POSTGRES_PORT),
             database: POSTGRES_DB,
             user: POSTGRES_USER,
             password: POSTGRES_PASSWORD
@@ -23,10 +24,10 @@ switch (ENV) {
     case 'test':
         exports.client = client = new pg_1.Pool({
             host: POSTGRES_HOST,
+            port: Number(POSTGRES_PORT),
             database: POSTGRES_DB_TEST,
             user: POSTGRES_USER,
             password: POSTGRES_PASSWORD
         });
         break;
 }
-console.log('Connected !!');
