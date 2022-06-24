bfb71c40680a3a3b37702a8d5ca4e506fa5515f4
Ahmad <ahmedmageed19@gmail.com>
2022-06-24 15:07:24 +0200

    EB CLI npm install fail

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,2 @@
 aws sts get-caller-identity
-aws s3 cp --recursive --acl public-read ./dist s3://elasticbeanstalk-us-east-1-139872294948/
+#aws s3 cp --recursive --acl public-read ./dist s3://elasticbeanstalk-us-east-1-139872294948/
