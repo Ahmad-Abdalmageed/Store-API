de8b1686795f5727cec16d559454f4662e701073
Ahmad <ahmedmageed19@gmail.com>
2022-06-25 01:46:19 +0200

    ci fixing

--- server/bin/deploy.sh
+++ server/bin/deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 echo "*******__ Store Server API  __*******"
-eb list
+
+echo "*******__ 1.Create a repository with the eb init command.....👨🏻‍💻🤌🏻  __*******"
+eb init store-api --platform node.js-16 --region us-east-1
+
+echo "*******__ 2.Sets the specified environment....👨🏻‍💻🤌🏻  __*******"
 eb use store-api-dev
-eb printenv
+
 echo "*******__ 3.Deploy the changes....👨🏻‍💻🤌🏻  __*******"
-eb deploy store-api-dev
+eb deploy
